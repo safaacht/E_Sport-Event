f55420ef707400825da7343090796b2488e652d0
safaacht <safaachtaoui@gmail.com>
2025-12-25 22:22:26 +0100

    changement en structure de code + ajout d' update

--- sponsor.php
+++ sponsor.php
@@ -2,24 +2,53 @@
 require_once './database.php';
 
 class Sponsor{
-    public string $name;
-    public int $ctrb_financiere;
-    public int $tournoi_id;
-    public $db;
+    private string $name;
+    private float $ctrb_financiere;
+    private int $tournoi_id;
+    private int $id;
 
-     public function __construct()
+     public function __construct($name,$ctrb_financiere	,$tournoi_id,$id=null)
     {
-        $this->db = new Database("localhost", "gestion_event", "root", "", 3307);
+        $this->name=$name;
+        $this->ctrb_financiere=$ctrb_financiere;
+        $this->tournoi_id=$tournoi_id;
+        $this->id=$id;
+    }
+    public function setName($name):void
+    {
+        $this->name=$name;
     }
 
-    public function getAll(){
-        $conn=$this->db->getConnection();
+    public function getName():string
+    {
+        return $this->name;
+    }
+
+    public function getContribution():float
+    {
+        return $this->ctrb_financiere;
+        
+    }
+
+    public function setContribution($ctrb_financiere):void
+    {
+        $this->ctrb_financiere=$ctrb_financiere;
+    }
+
+    public function setTournoiId( $tournoi_id):void
+    {
+        $this->tournoi_id=$tournoi_id;
+    }
+    public function getTournoiId():int
+    {
+        return $this->tournoi_id;
+    }
+    public function getAll($conn){
         $sql=mysqli_query($conn,"SELECT * FROM sponsor");
         return mysqli_fetch_all($sql,MYSQLI_ASSOC);
     }
 
-    public function create(){
-        $conn=$this->db->getConnection();
+    public function create($conn){
         $sql="INSERT INTO sponsor VALUES(?,?,?)";
         $stmt=mysqli_prepare($conn,$sql);
         mysqli_stmt_bind_param($stmt,"sii",$this->name,$this->ctrb_financiere,$this->tournoi_id);
@@ -27,10 +56,18 @@ class Sponsor{
         mysqli_stmt_close($stmt);
     }
 
-    public function delete(){
-        $conn = $this->db->getConnection();
+    public function delete($conn)
+    {
 
-        $sql="DELETE ON sponsor WHERE $this->name";
+        $sql="DELETE ON sponsor WHERE id=$this->id";
         mysqli_query($conn,$sql);
+    }
+    public function update($conn)
+    {
+        $stmt=mysqli_prepare($conn,"UPDATE sponsor SET name=?, contribution_finan=? tournoi_id=? WHERE id=?");
+        mysqli_stmt_bind_param($stmt,'ssi', $this->name,$this->ctrb_financiere ,$this->tournoi_id, $this->id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+
     }
 }
